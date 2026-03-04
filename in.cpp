36d55c14ead9176f9c5b755cfd8de8a20aae4ade
FC243401 <pl1627446@gmail.com>
2026-03-04 21:01:14 +0600

    Add files via upload

--- in.cpp
+++ in.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[100];
+
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Insertion Sort
+    for(int i = 1; i < n; i++) {
+        int key = arr[i];      // current element
+        int j = i - 1;
+
+        // shift elements greater than key
+        while(j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;      // insert at correct position
+    }
+
+    cout << "Sorted array: ";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
